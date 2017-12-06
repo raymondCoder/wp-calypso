544e9683487dc84fc20aa651202e8ceff4567045
Copons <copons@users.noreply.github.com>
2017-12-06 15:54:07 +0000

    Wire up back logic to post view back button

--- client/my-sites/comments/comment-list/index.jsx
+++ client/my-sites/comments/comment-list/index.jsx
@@ -51,6 +51,7 @@ export class CommentList extends Component {
 		comments: PropTypes.array,
 		deleteComment: PropTypes.func,
 		likeComment: PropTypes.func,
+		onBack: PropTypes.func,
 		recordBulkAction: PropTypes.func,
 		recordChangePage: PropTypes.func,
 		replyComment: PropTypes.func,
@@ -413,6 +414,7 @@ export class CommentList extends Component {
 			isCommentsTreeSupported,
 			isLoading,
 			isPostView,
+			onBack,
 			page,
 			postId,
 			siteBlacklist,
@@ -447,7 +449,7 @@ export class CommentList extends Component {
 				) }
 				{ isCommentsTreeSupported && <QuerySiteCommentsTree siteId={ siteId } status={ status } /> }
 
-				{ isPostView && <CommentListHeader postId={ postId } /> }
+				{ isPostView && <CommentListHeader onBack={ onBack } postId={ postId } /> }
 
 				<CommentNavigation
 					commentsPage={ commentsPage }
